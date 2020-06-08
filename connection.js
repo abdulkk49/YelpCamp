3041c9bbf5f98c7fdc9a9c4ffe8e7fb0cffd89a8
Ubuntu <ubuntu@ip-172-31-81-214.ec2.internal>
2020-06-08 07:27:26 +0000

    Make changes to connectionjs

--- connection.js
+++ connection.js
@@ -1,11 +1,11 @@
 var mongoose = require('mongoose');
 
-var URI = "mongodb+srv://abdulkk49:abdulkk49@yelpcamp-bkoyn.mongodb.net/yelp_camp?retryWrites=true&w=majority";
+// var URI = "mongodb+srv://abdulkk49:abdulkk49@yelpcamp-bkoyn.mongodb.net/yelp_camp?retryWrites=true&w=majority";
 
-const connectDB = async() => {
-  await mongoose.connect(URI, {
-      useUnifiedTopology : true,
-      useNewUrlParser : true
+const connectDB = function(){
+        mongoose.connect(process.env.DATABASEURL, {
+        useUnifiedTopology : true,
+        useNewUrlParser : true
   });
 };
 // mongoose.connect("mongodb://localhost/yelp_camp");
